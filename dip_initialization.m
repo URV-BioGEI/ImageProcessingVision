f304ac9ecb783c986c882ba2cd202dac32b406da
Aleix Mariné <aleix.marine@estudiants.urv.cat>
2018-12-20 12:45:59 +0100

    refilling of the tiles in the border of image almost done

---
+++ dip_initialization.m
@@ -0,0 +1,3 @@
+addpath('/lib/dip/common/dipimage');  % Adds path to diplib
+dip_initialise;  % Initilialize libraries
+dipsetpref('imagefilepath','/home/amt/IPV/');  % Optional: sets default image saving/loading folder
